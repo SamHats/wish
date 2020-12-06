16f2676ec827f7e355db62ff7aaaf2df6ab99ca7
Sam Clifford <s.clifford@zengenti.com>
2020-12-06 19:58:01 +0000

    + [layout] - minor

--- components/layout.js
+++ components/layout.js
@@ -8,7 +8,7 @@ export default function Layout({children}) {
       <div className="min-h-screen bg-gradient-to-r from-purple-400 via-purple-300 to-blue-400">
         <div className="min-h-screen bg-hero-texture--topography bg-opacity-25 pt-8">
           <Nav className="container mx-auto text-center" />
-          <div className="container mx-md m-auto max-w-4xl">
+          <div className="container mx-md m-auto max-w-4xl px-2">
             <main className="bg-white rounded-lg shadow-lg p-8 py-10">{children}</main>
           </div>
         </div>
